bb51c0b7442d0e353d91eab5ee0be74b11eca353
Hania <hkrzyska@gmail.com>
2019-05-28 16:23:49 +0200

    Websecurity config

--- src/main/java/com/alight/request_eval/config/WebSecurityConfig.java
+++ src/main/java/com/alight/request_eval/config/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -29,16 +28,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("rodent@rodent.pl")
                 .password("grain")
-                .roles("ADMIN");
+                .roles("ADMIN")
+                .and()
+                .withUser("user@rodent.pl")
+                .password("feed")
+                .roles("USER");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers("/administrator/**").hasRole("ROLE_ADMIN")
-                .antMatchers("/evaluator/**").hasRole("ROLE_USER")
-                .antMatchers("/h2/").hasRole("ROLE_ADMIN")
+                .antMatchers("/administrator/**").hasRole("ADMIN")
+                .antMatchers("/evaluator/**").hasRole("USER")
+                .antMatchers("/h2/**").hasRole("ADMIN")
                 .anyRequest().permitAll()
                 .and()
                 .csrf().disable()
