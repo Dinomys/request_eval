e221bd58935f2fa7386e400743a69cb5fee00b15
Hania <hkrzyska@gmail.com>
2019-06-02 13:28:01 +0200

    Added schema.sql

--- src/main/java/com/alight/request_eval/service/UserService.java
+++ src/main/java/com/alight/request_eval/service/UserService.java
@@ -1,7 +1,23 @@
 package com.alight.request_eval.service;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    public long getLoggedUserId (){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUser = authentication.getName();
+        long id = 0;
+        try {
+            id = Long.parseLong(currentUser);
+        } catch (Exception e){
+            authentication.setAuthenticated(false);
+        }
+        return id;
+    }
+
+
 }
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,3 @@
+INSERT INTO plan (id) VALUES ('May 2018');
+INSERT INTO plan (id) VALUES ('April 2018');
+INSERT INTO plan (id) VALUES ('March 2018');
