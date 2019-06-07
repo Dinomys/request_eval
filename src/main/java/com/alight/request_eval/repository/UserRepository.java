6de9149c34e33e51b7807641df37449892a3c088
Hania <hkrzyska@gmail.com>
2019-06-07 08:16:36 +0200

    Removed schema, removed query blocking the app

--- src/main/java/com/alight/request_eval/repository/UserRepository.java
+++ src/main/java/com/alight/request_eval/repository/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query("SELECT u FROM User u WHERE u.firstname = :firstname")
-    User findbyName(String name);
+/*    @Query("SELECT u FROM User u WHERE u.firstname = :firstname")
+    User findbyName(String name);*/
 
 }
